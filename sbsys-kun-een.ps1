4e9abbe162dec00ece412e9a5c3eda8c1dda7565
Dan Justice <109306080+danjustice2@users.noreply.github.com>
2025-04-09 08:39:16 +0200

    Updated to use the existing launcher script

--- sbsys-kun-een.ps1
+++ sbsys-kun-een.ps1
@@ -1,18 +1,20 @@
 # Load Windows Forms assembly
 Add-Type -AssemblyName System.Windows.Forms
 
-# Define the path to the executable
-$exePath = "C:\Program Files\SBSYS\SbSysDrift\Sbsys.Windows.Client.exe"
+# Define the path to the executable and parameters
+$exePath = "C:\Windows\System32\wscript.exe"
+$parameters = '"C:\Program Files\SBSYS\SBSYS.vbs" "\\srafil01v\prog\SBSYS\SBSYSstarter\SBSYS.INI" "SbSysDrift"'
+$sbsysExePath = "C:\Program Files\SBSYS\SbSysDrift\Sbsys.Windows.Client.exe"
 
 # Get the process name from the executable path
-$processName = [System.IO.Path]::GetFileNameWithoutExtension($exePath)
+$processName = [System.IO.Path]::GetFileNameWithoutExtension($sbsysExePath)
 
 # Check if the process is already running
 $process = Get-Process -Name $processName -ErrorAction SilentlyContinue
 
 if ($process -eq $null) {
-    # Process is not running, start the executable
-    Start-Process $exePath
+    # Process is not running, start the executable with parameters
+    Start-Process $exePath -ArgumentList $parameters
 } else {
     # Process is already running, show a popup message with options
     $result = [System.Windows.Forms.MessageBox]::Show("SBSYS kører allerede. Vil du afslutte programmet og starte den igen?", "Advarsel", [System.Windows.Forms.MessageBoxButtons]::YesNo, [System.Windows.Forms.MessageBoxIcon]::Warning)
@@ -20,7 +22,7 @@ if ($process -eq $null) {
     if ($result -eq [System.Windows.Forms.DialogResult]::Yes) {
         # User chose to end the process and start it again
         Stop-Process -Name $processName -Force
-        Start-Process $exePath
+        Start-Process $exePath -ArgumentList $parameters
     } else {
         # User chose not to end the process
     }
